d78e6c4d8154bb15afddcbbc4984e7fce41d4504
t0uu <srtouzet@hotmail.com>
2022-11-01 01:01:04 -0300

    add chapter

---
+++ src/Experience.js
@@ -0,0 +1,50 @@
+import { Text,Html,ContactShadows,PresentationControls,Environment,Float, useGLTF } from '@react-three/drei'
+export default function Experience()
+{
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf');
+    return <>
+    <Environment preset='city'/>
+    <color args={['#695b5b']} attach="background"/>
+    {/* <OrbitControls makeDefault />
+     */}
+     <PresentationControls global
+     rotation={[0.13,0.1,0]}
+    //  polar={[-0.4,0.2]}
+    //  azimuth={[-1,0.75]}
+    //  config={{mass:2,tension:400}}
+    //  snap={{mass:4,tension:400}}
+     >
+
+<Float rotationIntensity={0.4}>
+        <rectAreaLight
+        width={2.5}
+        height={1.65}
+        intensity={65}
+        color={'ivory'}
+        rotation={[0.1,Math.PI,0]}
+        position={[0,0.55,-1.15]}
+        />
+        <primitive object={computer.scene}
+        position-y={-1.2}
+        >
+            <Html 
+            transform
+            wrapperClass='htmlScreen'
+            distanceFactor={1.17}
+            position={[0,1.56,-1.4]}
+            rotation-x={-0.256}
+            >
+                <iframe src='https://www.dropoversized.com.ar/'/></Html>
+        </primitive>
+        </Float>
+        <Text font='Inter, sans-serif' fontSize={1} position={[3,0.75,0.75]}
+        rotation-y={-1.25}
+        maxWidth={3}
+        textAlign={'center'}
+        color={'#000'}
+        >drøp </Text>
+        </PresentationControls>
+   <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4}/>
+    </>
+
+}
